fdfd1ab1a612d1da1d4365384c57514aa61d0125
Humberto Muguerza <hmquiroz1@hotmail.com>
2025-03-30 07:14:43 -0500

    30032025

--- 3. Variables y Tipos de Datos/7.MetodosCadenas.py
+++ 3. Variables y Tipos de Datos/7.MetodosCadenas.py
@@ -4,5 +4,7 @@ print(texto)
 print(texto.lower())
 print(texto.upper())
 print(len(texto))
+#Strip: Elimina un caracter especifico
 print(texto.strip("-"))
+#Split: Divide una cadena en base a un caracter
 print(texto.split("o"))
---
+++ 3. Variables y Tipos de Datos/8.OperadoresCadenas.py
@@ -0,0 +1,12 @@
+cadena = "Esta es una cadena"
+cadena2 = " que va a unificarse en una sola"
+
+nombre = "Walter Coto"
+
+print(cadena)
+print(cadena2)
+print(cadena + cadena2)
+print(cadena * 3)
+
+print("Hola mi nombre es:" + nombre)
+print("Hola mi nombre es:", nombre)
